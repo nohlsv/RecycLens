bcc341dc5d537711d92c732362152ba76667d249
Arkin Reinier <arkinreinier616@gmail.com>
2025-10-19 13:39:14 +0800

    Update frontend

--- app/src/main/java/com/example/recyclens/MainActivity.kt
+++ app/src/main/java/com/example/recyclens/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
