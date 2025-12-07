eb142e4d56e825d7bc8190a8a6fbd2bc2fcf7fad
snoopypop <percy.silvala.cics@ust.edu.ph>
2025-12-07 18:19:37 +0800

    updated

--- app/src/main/java/com/example/recyclens/ScannerActivity.kt
+++ app/src/main/java/com/example/recyclens/ScannerActivity.kt
@@ -133,7 +133,7 @@ class ScannerActivity : AppCompatActivity() {
 
         initModels()
         ensureCameraReady()
-        setupBottomBar()
+        setupBottomBar(BottomBar.Tab.PLAY)
         setupLanguageToggle()
         setupButtons()
 
